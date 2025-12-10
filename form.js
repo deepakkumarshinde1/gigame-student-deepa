1e3fd8f5438c2247f662114d18d8322eabcfda28
deepakkumarshinde1 <deepakkumar.shinde1@gmail.com>
2025-12-10 07:39:58 +0530

    formData

--- form.js
+++ form.js
@@ -1,15 +1,12 @@
 let aForm = document.querySelector("#form");
 aForm.addEventListener("submit", (event) => {
   event.preventDefault();
-  let formData = new FormData(aForm);
-  let myData = {};
-  formData.forEach((value, name) => {
-    myData[name] = value;
-  });
+  const formData = new FormData(aForm);
+  const myData = Object.fromEntries(formData);
 
   fetch("/login", {
     method: "POST",
     body: formData,
-  });
-  console.log("form submitted");
+    });
+  console.log("form submitted", myData);
 });
